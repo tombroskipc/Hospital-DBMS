f3e70fb793f010a781936c32d26e1c7dba480b46
tombroskipc <tombroskipc@gmail.com>
2021-10-31 16:23:40 +0700

    create base and template

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def main_index():
-    return '<h1>Hello view!</h1>'
+    return render_template('index.html')
